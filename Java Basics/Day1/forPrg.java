b6456e55d40332459d39667100c81c72703d2474
steve <steveprathik.fernandes2001@gmail.com>
2023-04-20 16:55:13 +0530

    updated programs

---
+++ Java Basics/Day1/forPrg.java
@@ -0,0 +1,14 @@
+class forPrg{
+    public static void main(String args[]){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                System.out.println(i+"----"+j);
+            }
+        }
+        int n=10,i=0;
+        while(n!=0){
+            System.out.println(i++);
+            n--;
+        }
+    }
+}
