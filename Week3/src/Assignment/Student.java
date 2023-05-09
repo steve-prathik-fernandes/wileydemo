b035932831eecd4e2c155ed6f9f3323261f1200b
steve <steveprathik.fernandes2001@gmail.com>
2023-05-09 21:31:51 +0530

    assignment changes

--- Week3/src/Assignment/Student.java
+++ Week3/src/Assignment/Student.java
@@ -11,6 +11,9 @@ public class Student
     char grade;
     public Student(String name,int problems_solved,char grade){
 
+        this.name=name;
+        this.problems_solved=problems_solved;
+        this.grade=grade;
     }
 
     public String getName() {
@@ -28,12 +31,15 @@ public class Student
     public static void main(String[] args) {
         List<Student>studentList=Arrays.asList(
           new Student("Steve",25,'A'),
-                new Student("Anil",30,'A')
+                new Student("Anil",40,'A'),
+                new Student("Janavi",50,'C'),
+                new Student("Dhanush",60,'A')
         );
 
-        studentList.stream().filter(s->s.getProblems_solved()>=30)
-                .limit(2)
-                .forEach(data-> System.out.println(data.getName()+" "+data.getProblems_solved()+" "+data.getGrade()));
+        studentList.stream().filter(s->s.getProblems_solved()>=20)
+                .forEach(stu-> {
+                    System.out.println(stu.getName() + " " + stu.getProblems_solved() + " " + stu.getGrade());
+                });
 
     }
 }
